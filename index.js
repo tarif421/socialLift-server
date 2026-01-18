6e34e7c9cad4e2af1cdcfbcc1d1a54d9c692ce79
Tariful Hasan <tarif305@gmail.com>
2026-01-18 15:41:11 +0600

    upcoming event data added

--- index.js
+++ index.js
@@ -1,38 +1,48 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = 3000;
 app.use(cors());
 app.use(express.json());
 
+const uri =
+  "mongodb+srv://socialLift-db:sTjp9E0VeKXbWjs5@cluster0.9aos02c.mongodb.net/?appName=Cluster0";
+
 // mongoDB
 
-const uri = "mongodb+srv://socialLift-db:sTjp9E0VeKXbWjs5@cluster0.9aos02c.mongodb.net/?appName=Cluster0";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const db = client.db("socialLift");
+    const modelCollection = db.collection("upcomingEvents");
+    // upcoming events
+    app.get("/socialLift", async (req, res) => {
+      const result = await modelCollection.find().toArray();
+      console.log(result);
+
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 app.get("/", (req, res) => {
   res.send("server is running fine");
