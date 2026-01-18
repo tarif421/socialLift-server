84c2d009c40039bcf94c73204f08e3d4fc599474
Tariful Hasan <tarif305@gmail.com>
2026-01-18 13:14:30 +0600

    mongo db connected

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = 3000;
 app.use(cors());
 app.use(express.json());
 
+// mongoDB
+
+const uri = "mongodb+srv://socialLift-db:sTjp9E0VeKXbWjs5@cluster0.9aos02c.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
