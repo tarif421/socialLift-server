b918fd063a648065a4ae6672459ef792d147f9bd
Tariful Hasan <tarif305@gmail.com>
2026-01-18 11:55:52 +0600

    project set up

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
+app.use(cors());
+app.use(express.json());
+
+
 
 app.get("/", (req, res) => {
   res.send("server is running fine");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
